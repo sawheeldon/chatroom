8c269972a8810df9285da15edc7e311f30c966c0
Simon <simon@jampot.design>
2016-08-28 17:21:10 +0000

    chat set up, logs to server cant pull down data and log it in the html

--- public/main.js
+++ public/main.js
@@ -1,23 +1,79 @@
 $(document).ready(function() {
     var socket = io();
-    var input = $('input');
-    var messages = $('#messages');
-
-    var addMessage = function(message) {
-        messages.append('<div>' + message + '</div>');
-    };
-
-    input.on('keydown', function(event) {
-        if (event.keyCode != 13) {
-            return;
-        }
+    var chatUsername = $('#chat-username');
+    var chatMessage = $('#chat-message');
+    // var input = $('#chat-message');
+    // var messages = $('#messages');
+    // var messageContainer = $('#message-container');
+    // var chatUserName = $('#chat-username');
+    // var submitButton = $('#chat-submit');
     
-        var message = input.val();
-        addMessage(message);
-        socket.emit('message', message);
-        input.val('');
-    });
-    
-    socket.on('message', addMessage);
+  socket.on('connect', function() {
+    var chatForm = document.forms.chatForm;
+
+     if (chatForm) {
+        
+        chatForm.addEventListener('submit', function(e) {
+          e.preventDefault();
+          
+          socket.emit('postMessage', {
+              username: chatUsername.value,
+              message: chatMessage.value,
+          });
+          chatMessage.value='';
+          chatMessage.focus();
+        }); //chatform event
+     socket.on('updateMessages', function(data) {
+          showMessage(data);
+        }); //updateMessages
+  } //chatform
+}); //socket
+
+function showMessage(data) {
+  var chatDisplay = $('.chat-display');
+  var newMessage = document.createElement('p');
+
+  if (chatUsername.value == data.username) {
+    newMessage.className = 'bg-success chat-text';
+  } else {
+    newMessage.className = 'bg-info text-warning chat-text';
+  }
+
+  newMessage.innerHTML = '<strong>' + data.username + '</strong>: ' + data.message;
+  chatDisplay.insertBefore(newMessage, chatDisplay.firstChild);
+}
+
 
 });
+
+//socket.on('updateMessages', showMessage);
+    
+//     var addMessage = function(message) {
+//         var newMessage = document.createElement('p');
+//         newMessage.innerHTML = '<strong>' + message.chatUserName + '</strong>: ' + message.input;
+        
+//         messageContainer.append('<div>' + message + '</div>');
+//     };
+
+//     input.on('keydown', function(event) {
+//         if (event.keyCode != 13) {
+//             console.log("Pressed Enter");
+//             return;
+//         }
+//         // else if ($(submitButton).click){
+//         //     console.log("Clicked");
+//         //     return;
+//         // };  
+    
+//         var message = input.val();
+//         addMessage(message);
+//         socket.emit('message',{
+//          username:chatUserName.value, 
+//          message:input.value,
+//         });
+//         input.val('');
+//     });
+    
+//     socket.on('message', addMessage);
+
+// });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var socket_io = require('socket.io');
 var http = require('http');
 var express = require('express');
+// bootstrap = require('bootstrap')
 
 var app = express();
 app.use(express.static('public'));
@@ -8,12 +9,23 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
+// io.attach(server);
+// io.on('connection', function(socket) {
+//   socket.on('postMessage', function(data) {
+//     io.emit('updateMessages', data);
+//   });
+// });
+
 io.on('connection', function (socket) {
     console.log('Client connected');
+    
+    socket.on ('disconnect', function (){
+        console.log('Client Disconnected');
+    });
 
-    socket.on('message', function(message) {
-        console.log('Received message:', message);
-        socket.broadcast.emit('message', message);
+    socket.on('postMessage', function(data) {
+        console.log('Received message:', data);
+        socket.broadcast.emit('updateMessages', data);
     });
 });
 server.listen(8080);
